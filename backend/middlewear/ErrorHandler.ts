e1c20dc23e475f3825ade368df791187adcae575
Benedict <benedict.arowo@gmail.com>
2024-04-06 05:41:00 +0300

    Add error handling middleware and wrapper function

---
+++ backend/middlewear/ErrorHandler.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from "express";
+
+export const ErrorHandler = (
+	err: any,
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	process.env.NODE_ENV === "development" && console.log(err);
+	res.status(err.status || 500);
+	return res.json({
+		message: err.message,
+		error: err,
+	});
+};
