9a5a1a50d92e2acb39861f86418b23da7e6317a5
Benedict <benedict.arowo@gmail.com>
2024-04-06 05:41:23 +0300

    Add error handling middleware and update package dependencies

--- backend/index.ts
+++ backend/index.ts
@@ -1,3 +1,4 @@
+import { ErrorHandler } from "./middlewear/ErrorHandler.js";
 import Routes from "./routes/index.js";
 const express = require("express");
 require("dotenv").config({ path: "../.env" });
@@ -5,7 +6,10 @@ require("dotenv").config({ path: "../.env" });
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
 Routes(app);
+
+app.use(ErrorHandler);
 
 app.listen(PORT, () => {
 	console.log("Server is running on port " + PORT);
