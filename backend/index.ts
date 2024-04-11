7be8148050c27b9fc8b4d27ad3254514662efabf
syazell1 <mafty.erin01@gmail.com>
2024-04-11 19:57:43 +0800

    added cors

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,18 @@
 import { ErrorHandler } from "./middlewear/ErrorHandler.js";
 import Routes from "./routes/index.js";
+import cors from 'cors'
 const express = require("express");
 const cookieParser = require("cookie-parser");
-require("dotenv").config({ path: "../.env" });
+require("dotenv").config({ path: ".env" });
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors({
+	origin: process.env.CLIENT_URL,
+	credentials : true,
+	maxAge: 3600
+}));
 app.use(express.json());
 app.use(cookieParser());
 Routes(app);
