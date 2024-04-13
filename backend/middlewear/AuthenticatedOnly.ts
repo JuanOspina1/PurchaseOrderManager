c55c216994332d1258ec6bc31231e859dff4b9e1
syazell1 <mafty.erin01@gmail.com>
2024-04-13 13:21:42 +0800

    update auth middleware for bearer token

--- backend/middlewear/AuthenticatedOnly.ts
+++ backend/middlewear/AuthenticatedOnly.ts
@@ -11,45 +11,16 @@ const AuthenticatedOnly = async (
 	res: Response,
 	next: NextFunction
 ) => {
+
+	const bearerHeader = req.headers['authorization'];
+
 	try {
-		let accessToken = req.cookies.access_token;
-		let refreshToken = req.cookies.refresh_token;
+		if (typeof bearerHeader !== 'undefined') {
+			const bearer = bearerHeader.split(' ');
+			const token = bearer[1];
 
-		// If access token is missing or expired, try refreshing it with the refresh token
-		if (!accessToken) {
-			process.env.NODE_ENV === "development" &&
-				console.log("Trying refresh token");
-			if (!refreshToken) {
-				throw new ErrorWithStatus(
-					StatusCodes.UNAUTHORIZED,
-					"Access token and refresh token are missing."
-				);
-			}
-
-			const decodedRefreshToken = jwt.verify(
-				refreshToken,
-				process.env.JWT_REFRESH_SECRET
-			);
-
-			// TODO: blacklisting? expiry?
-			const userId = decodedRefreshToken.userId;
-			req.user = { id: userId };
-
-			accessToken = generateAccessToken(userId);
-
-			res.cookie("access_token", accessToken, {
-				httpOnly: true,
-				expires: new Date(
-					Date.now() +
-						parseInt((process.env.ACCESS_COOKIE as unknown as string) ?? 0)
-				),
-			});
-		} else {
-			// Verify access token
-			process.env.NODE_ENV === "development" &&
-				console.log("Trying access token");
 			const decodedAccessToken = jwt.verify(
-				accessToken,
+				token,
 				process.env.JWT_ACCESS_SECRET
 			);
 			req.user = { id: decodedAccessToken.userId };
@@ -66,13 +37,13 @@ const AuthenticatedOnly = async (
 			req.user = user;
 			next();
 		}
-	} catch (error) {
-		process.env.NODE_ENV === "development" &&
-			console.error("Authentication error:", error);
+	}
+	catch (err) {
 		next(
 			new ErrorWithStatus(StatusCodes.UNAUTHORIZED, "Authentication failed.")
 		);
 	}
-};
+}
+
 
 export default AuthenticatedOnly;
