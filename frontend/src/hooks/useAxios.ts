fcc772438949be0113da8766fdd4bb631b5bf40b
syazell1 <mafty.erin01@gmail.com>
2024-04-13 17:11:26 +0800

    update axios hooks and to use useUser

--- frontend/src/hooks/useAxios.ts
+++ frontend/src/hooks/useAxios.ts
@@ -2,9 +2,10 @@ import { UserContext } from "@/contexts/user-context"
 import { client } from "@/lib/axios"
 import { useContext, useEffect } from "react"
 import { useRefreshToken } from "./useRefreshToken";
+import { useUser } from "./use-user";
 
 export const useAxios = () => {
-    const userContext = useContext(UserContext);
+    const {accessToken, setIsLoading} = useUser();
     const refresh = useRefreshToken();
 
     useEffect(() =>{
@@ -12,7 +13,7 @@ export const useAxios = () => {
             config => {
                 if(!config.headers["Authorization"]) {
 
-                    config.headers["Authorization"] = `Bearer ${userContext?.user.accessToken}`
+                    config.headers["Authorization"] = `Bearer ${accessToken}`
                 }
 
                 return config;
@@ -24,19 +25,18 @@ export const useAxios = () => {
         async (err) => {
             const prevRequest = err?.config;
 
-            if(err?.config?.status === 401 && !prevRequest?.sent) {
+            if(err?.response?.status === 401 && !prevRequest?.sent) {
                 prevRequest.sent = true;
                 const token = await refresh();
 
-
                 if(token !== undefined && token.length > 0) {
                     prevRequest.headers["Authorization"] = `Bearer ${token}`;
-                    userContext?.setIsLoading(false);
+                    setIsLoading(false);
                     return client(prevRequest);
                 }
             }
 
-            userContext?.setIsLoading(false);
+            setIsLoading(false);
             return Promise.reject(err);
         }
        ) 
--- frontend/src/hooks/useRefreshToken.ts
+++ frontend/src/hooks/useRefreshToken.ts
@@ -1,12 +1,17 @@
 import axios from "@/lib/axios"
 import { AuthResponse } from '@/types/auth/AuthResponse'
 import { isAxiosError } from "axios";
+import { useUser } from "./use-user";
 
 export const useRefreshToken = () => {
+    const {setAccessToken, setDetails} = useUser();
     const refresh = async () => {
 
         try {
             const res = await axios.get<AuthResponse>('/refresh', { withCredentials: true })
+
+            setAccessToken(res.data.accessToken);
+            setDetails(res.data.data);
 
             return res.data.accessToken;
         } catch (err) {
