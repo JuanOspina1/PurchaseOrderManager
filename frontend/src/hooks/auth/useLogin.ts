1eac484755e156550baa1f078538e1e6212ffd0b
syazell1 <mafty.erin01@gmail.com>
2024-04-13 17:10:56 +0800

    update user login hook

--- frontend/src/hooks/auth/useLogin.ts
+++ frontend/src/hooks/auth/useLogin.ts
@@ -3,20 +3,22 @@ import axios from "@/lib/axios"
 import { useMutation } from "@tanstack/react-query"
 import { useUser } from "../use-user";
 import { useRouter } from "next/navigation";
+import { AuthResponse } from "@/types/auth/AuthResponse";
 
 export const useLogin = () => {
-//   const { checkSession } = useUser();
+  const { setAccessToken, setDetails} = useUser();
   const router = useRouter();
 
     return useMutation({
         mutationFn: async (input : SignInWithPasswordParams) => {
-            const res = await axios.post('/login', input, {withCredentials : true});
+            const res = await axios.post<AuthResponse>('/login', input, {withCredentials : true});
 
             return res.data;
         },
-        onSuccess: async () => {
+        onSuccess: async ({accessToken, data}) => {
             // await checkSession?.();
-
+            setAccessToken(accessToken)
+            setDetails(data);
             router.push('/dashboard')
         }
     })
