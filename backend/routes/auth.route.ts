c9cd649df0c93e22928ba6405413e1a20cb3d4d0
Benedict <benedict.arowo@gmail.com>
2024-04-13 11:41:36 +0300

    Add main_company route and controller

--- backend/routes/auth.route.ts
+++ backend/routes/auth.route.ts
@@ -10,10 +10,6 @@ import AuthenticatedOnly from "../middlewear/AuthenticatedOnly";
 const express = require("express");
 const Router = express.Router();
 
-Router.route("/").get((req: Request, res: Response) => {
-	res.send("Hello World Auth");
-});
-
 Router.route("/login").post(Wrapper(loginController));
 // Router.route("/register").post(Wrapper(registerController));
 Router.route("/logout").post(AuthenticatedOnly, Wrapper(logoutController));
